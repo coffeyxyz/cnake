5acd1c76774ae572d5d12394ea8e0aaea6d95e7a
foggynight <foggynight709@gmail.com>
2020-08-07 20:42:37 -0230

    Update default game config

--- src/includes.h
+++ src/includes.h
@@ -1,9 +1,9 @@
-#define SHEIGHT	22          // Screen height.
-#define SWIDTH	78          // Screen width.
+#define SHEIGHT    21 // Screen height.
+#define SWIDTH     78 // Screen width.
 
-#define FRAMERATE   10      // Number of frames per second, also, gamespeed.
+#define FRAMERATE  10 // Number of frames per second, also, gamespeed.
 
-#define STARTFOOD   3       // Pieces of food on screen at the start.
-#define	MAXFOOD		20      // Maximum pieces of food allowed on screen.
-#define FOOD_TIME	5       // Number of seconds between each food spawn.
-#define	SCORE_TIME	1       // Number of seconds between incrementing score.
+#define STARTFOOD  3  // Pieces of food on screen at the start.
+#define MAXFOOD    5  // Maximum pieces of food allowed on screen.
+#define FOOD_TIME  1  // Number of seconds between each food spawn.
+#define SCORE_TIME 1  // Number of seconds between incrementing score.
