1b5a8d65d5232482876452277f10046f443238c8
foggynight <foggynight709@gmail.com>
2021-05-19 14:41:33 -0230

    Reorder macros in alphabetical order

--- src/config.h
+++ src/config.h
@@ -1,13 +1,13 @@
 // Copyright (C) 2020-2021 Robert Coffey
 // Released under the GPLv2 license
 
-#define SCR_WIDTH	20	// Game grid width
-#define SCR_HEIGHT	10	// Game grid height
+#define FOOD_MAX	1	// Maximum pieces of food allowed on screen
+#define FOOD_START	1	// Pieces of food on screen at the start
 
 #define FRAME_RATE	10	// Number of frames per second, also, gamespeed
 
-#define FOOD_START	1	// Pieces of food on screen at the start
-#define FOOD_MAX	1	// Maximum pieces of food allowed on screen
+#define SCR_HEIGHT	10	// Game grid height
+#define SCR_WIDTH	20	// Game grid width
 
 #define TIME_FOOD	0	// Number of seconds between each food spawn
 #define TIME_SCORE	1	// Number of seconds between incrementing score
