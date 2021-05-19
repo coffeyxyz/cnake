b1e4312d11cd2861624f970b509aadf9b584ee88
foggynight <foggynight709@gmail.com>
2021-05-19 13:44:31 -0230

    Convert indentation to tabs

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /* --- cnake ---
  *
- * Classic snake game, but with a c.
+ * Classic snake game, but with a C.
  *
  * Copyright (C) 2020 Robert Coffey
  * Released under the GPLv2 license */
@@ -11,15 +11,15 @@
 #include "config.h"
 #include "engine.h"
 
-#define FRAMESLEEP(X) usleep(1000000/X)
+#define FRAMESLEEP(X)	usleep(1000000/X)
 
 int main(void)
 {
-    engine_setup();
+	engine_setup();
 
-    while (engine_step())
-        FRAMESLEEP(FRAMERATE);
+	while (engine_step())
+		FRAMESLEEP(FRAMERATE);
 
-    printf("Gameover! Your final score was: %ld\n", engine_kill());
-    return 0;
+	printf("Gameover! Your final score was: %ld\n", engine_kill());
+	return 0;
 }
--- src/screen.c
+++ src/screen.c
@@ -8,46 +8,46 @@
 
 void screen_setup(void)
 {
-    initscr();
+	initscr();
 
-    cbreak();
-    noecho();
-    nodelay(stdscr, true);
-    curs_set(0);
+	cbreak();
+	noecho();
+	nodelay(stdscr, true);
+	curs_set(0);
 
-    move(0,0);
-    printw("Score: ");
+	move(0,0);
+	printw("Score: ");
 
-    move(1,0);
-    for (int i = 0; i < SWIDTH+2; ++i) {
-        addch('#');
-    }
-    move(SHEIGHT+2, 0);
-    for (int i = 0; i < SWIDTH+2; ++i) {
-        addch('#');
-    }
-    for (int i = 0; i < SHEIGHT; ++i) {
-        mvaddch(i+2, 0, '#');
-        mvaddch(i+2, SWIDTH+1, '#');
-    }
+	move(1,0);
+	for (int i = 0; i < SWIDTH+2; ++i) {
+		addch('#');
+	}
+	move(SHEIGHT+2, 0);
+	for (int i = 0; i < SWIDTH+2; ++i) {
+		addch('#');
+	}
+	for (int i = 0; i < SHEIGHT; ++i) {
+		mvaddch(i+2, 0, '#');
+		mvaddch(i+2, SWIDTH+1, '#');
+	}
 }
 
 void screen_update(char chars[SHEIGHT][SWIDTH], int score)
 {
-    move(0, 7);
-    printw("%d", score);
+	move(0, 7);
+	printw("%d", score);
 
-    for (int row = 0; row < SHEIGHT; ++row) {
-        move(row+2, 1);
-        for (int col = 0; col < SWIDTH; ++col) {
-            addch(chars[row][col]);
-        }
-    }
+	for (int row = 0; row < SHEIGHT; ++row) {
+		move(row+2, 1);
+		for (int col = 0; col < SWIDTH; ++col) {
+			addch(chars[row][col]);
+		}
+	}
 
-    refresh();
+	refresh();
 }
 
 void screen_kill(void)
 {
-    endwin();
+	endwin();
 }
